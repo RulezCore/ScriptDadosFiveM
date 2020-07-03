4a766b707fba71ae40ea3d4ff1845c69d13d3cb3
Daniel Casado <ixrulez69@gmail.com>
2020-07-03 20:09:02 +0200

    akdjak

--- client.lua
+++ client.lua
@@ -1,6 +1,9 @@
 RegisterCommand('dados', function(source, args, rawCommand)
     if args[1] and args[3] then
         print('Los argumentos son: ' .. args[1] .. '-' .. args[3])
+    else 
+        print('Comeme un huevo')
+    end
     
     end, false
 )
