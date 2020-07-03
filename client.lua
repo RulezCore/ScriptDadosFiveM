b1220eb2554d3245132f40e164a2b495a351fa42
Daniel Casado <ixrulez69@gmail.com>
2020-07-03 20:18:49 +0200

    Type

--- client.lua
+++ client.lua
@@ -2,7 +2,7 @@ RegisterCommand('dados', function(source, args, rawCommand)
     theArgs = args[1]
 
     if theArgs then
-        print(theArgs)
+        print(type(theArgs))
     else 
         print('Comeme un huevo')
     end
