2fa54b53f9a44b40499b747aa98c47cc406076bc
YauSpain <jaime003gaming@gmail.com>
2020-07-03 20:41:20 +0200

    end del for

--- client.lua
+++ client.lua
@@ -4,6 +4,7 @@ RegisterCommand('dados', function(source, args, rawCommand)
     if theArgs then
         for num in theArgs:gmatch('[^-%s]+') do
             print(num .. '/')
+        end
     else 
         print('Comeme un huevo')
     end
