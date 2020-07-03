abd16a896086af9b95f0a84d31e1f78573404de9
Daniel Casado <ixrulez69@gmail.com>
2020-07-03 20:53:43 +0200

    Table insert

--- client.lua
+++ client.lua
@@ -2,8 +2,9 @@ RegisterCommand('dados', function(source, args, rawCommand)
     theArgs = args[1]
 
     if theArgs then
-        for num in theArgs:gmatch('[^-%s]+') do
-            nums[num]
+        nums = {}
+        for i,num in theArgs:gmatch('[^-%s]+') do
+            table.insert(nums, num)
         end
 
         print('Numero 1: ' .. nums[0] .. 'Numero 2: ' .. nums[1])
