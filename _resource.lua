cd8f00a5ff24795beeb18fd3689124fa5bc6d1db
Daniel Casado <ixrulez69@gmail.com>
2020-07-03 18:44:34 +0200

    commit

--- _resource.lua
+++ _resource.lua
@@ -5,6 +5,6 @@ author 'RulezCore y Yau'
 description 'Script to use the command /dados'
 version '0.1'
 
-resource_type 'gametype' { name = 'My awesome game type!' }
+resource_type 'gametype' { name = 'My awesome game type!!' }
 
 client_script 'mymode_client.lua'
