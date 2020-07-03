e55cd20b97a531449750493c8a941ae98243e45e
Daniel Casado <ixrulez69@gmail.com>
2020-07-03 20:58:21 +0200

    Hola

--- client.lua
+++ client.lua
@@ -2,12 +2,12 @@ RegisterCommand('dados', function(source, args, rawCommand)
     theArgs = args[1]
 
     if theArgs then
-        nums = {}
-        for i,num in theArgs:gmatch('[^-%s]+') do
-            table.insert(nums, num)
+        numeros = {}
+        for num in theArgs:gmatch('[^-%s]+') do
+            table.insert(numeros, num)
         end
 
-        print('Numero 1: ' .. nums[0] .. 'Numero 2: ' .. nums[1])
+        print(numeros)
     else 
         print('Comeme un huevo')
     end
