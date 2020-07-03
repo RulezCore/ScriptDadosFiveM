dc112e256f2c4a50fcb88b81bf9517e8015c7881
Daniel Casado <ixrulez69@gmail.com>
2020-07-03 20:32:12 +0200

    Split v2

--- client.lua
+++ client.lua
@@ -2,9 +2,8 @@ RegisterCommand('dados', function(source, args, rawCommand)
     theArgs = args[1]
 
     if theArgs then
-        num1, num2 = theArgs:match("([^-]+),([^-]+)")
-        print(num1)
-        print(num2)
+        local result = mw.text.split(theArgs,"-")
+        print(result[0] .. result[1])
     else 
         print('Comeme un huevo')
     end
