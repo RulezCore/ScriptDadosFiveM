05dccb43bc1c670b5c836cd617a670ab2337cf4d
Daniel Casado <ixrulez69@gmail.com>
2020-07-03 20:07:27 +0200

    ahora si

--- client.lua
+++ client.lua
@@ -1,6 +1,8 @@
 RegisterCommand('dados', function(source, args, rawCommand)
     if args[1] and args[3] then
         print('Los argumentos son: ' .. args[1] .. '-' .. args[3])
+    
+    end, false
 )
 
 
